a10d1f8a78fae24a6e1df61576d28534d10ef9db
hisasann <hisasann25@gmail.com>
2019-01-30 05:24:14 +0900

    first commit

---
+++ components/Logo.vue
@@ -0,0 +1,8 @@
+<template>
+  <p align="center">
+    <img src="https://github.com/lemon-sour/lemon-sour.js/blob/master/internals/images/lemon-sour.jpg?raw=true" alt="lemon-sour.jpg" width="200">
+  </p>
+</template>
+
+<style>
+</style>
