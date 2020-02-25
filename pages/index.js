a160c5ef071dd5dc9472c9150a44914beff9bb93
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-02-25 12:31:38 +0900

    🥓 feat: add to get logo image from contentful

--- pages/index.js
+++ pages/index.js
@@ -1,242 +1,259 @@
 import Head from 'next/head'
+import { useState } from "react"
+import * as contentful from 'contentful'
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>lemon-sour.js</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const client = contentful.createClient({
+  space: "ra794i9t0yln",
+  accessToken: "U1Wffn5NIhptGsWKyliQCILCWpoaCjJK6mwJwYO_58g"
+});
 
-    <main>
-      <p align="center">
-        <img src="/lemon-sour.small.jpg" />
-      </p>
+const Home = () => {
+  const [logo, setLogo] = useState('');
 
-      <h1 className="title">
-        Welcome to <a href="https://lemon-sourjs-site.netlify.com/">lemon-sour.js!</a>
-      </h1>
+  client.getEntry('dLKyuqKHoJhh2bc5Xypqf')
+    .then((entry) => {
+      setLogo(entry.fields.image.url)
+    })
+    .catch(console.error)
 
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
+  return (
+    <div className="container">
+      <Head>
+        <title>lemon-sour.js</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-      <div className="links">
+      <main>
+        <p align="center">
+          <img src={logo} />
+        </p>
+
+        <h1 className="title">
+          Welcome to <a href="https://lemon-sourjs-site.netlify.com/">lemon-sour.js!</a>
+        </h1>
+
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+
+        <div className="links">
+          <a
+            href="https://github.com/lemon-sour/lemon-sour.js"
+            target="_blank"
+            className="button--grey"
+          >
+            GitHub
+          </a>
+        </div>
+
+        <div className="grid">
+          <a href="https://github.com/lemon-sour/docs" className="card">
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about lemon-sour.js features and API.</p>
+          </a>
+
+          <a
+            href="https://github.com/lemon-sour/lemon-sour.js/tree/master/example"
+            className="card"
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example lemon-sour.js projects.</p>
+          </a>
+        </div>
+      </main>
+
+      <footer>
         <a
-          href="https://github.com/lemon-sour/lemon-sour.js"
+          href="https://github.com/hisasann"
           target="_blank"
-          className="button--grey"
+          rel="noopener noreferrer"
         >
-          GitHub
+          Powered by <img src="/hisasann_400x400.jpg" alt="ZEIT Logo" className="logo" />
         </a>
-      </div>
+      </footer>
 
-      <div className="grid">
-        <a href="https://github.com/lemon-sour/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about lemon-sour.js features and API.</p>
-        </a>
-
-        <a
-          href="https://github.com/lemon-sour/lemon-sour.js/tree/master/example"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example lemon-sour.js projects.</p>
-        </a>
-      </div>
-    </main>
-
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <a href="https://github.com/hisasann"><img src="/hisasann_400x400.jpg" alt="ZEIT Logo" className="logo" /></a>
-      </a>
-    </footer>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        margin: 0 auto;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
-      }
-
-      main {
-        padding: 1rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .links {
-        padding-top: 15px;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          margin: 0 auto;
+          padding: 0 0.5rem;
+          display: flex;
           flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          text-align: center;
         }
-      }
-    `}</style>
 
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
+        main {
+          padding: 1rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
 
-      * {
-        box-sizing: border-box;
-      }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
 
-      .button--green {
-        display: inline-block;
-        border-radius: 4px;
-        border: 1px solid #3b8070;
-        color: #3b8070;
-        text-decoration: none;
-        padding: 10px 30px;
-      }
+        footer img {
+          margin-left: 0.5rem;
+        }
 
-      .button--green:hover {
-        color: #fff;
-        background-color: #3b8070;
-      }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
 
-      .button--grey {
-        display: inline-block;
-        border-radius: 4px;
-        border: 1px solid #35495e;
-        color: #35495e;
-        text-decoration: none;
-        padding: 10px 30px;
-        margin-left: 15px;
-      }
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
 
-      .button--grey:hover {
-        color: #fff;
-        background-color: #35495e;
-      }
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
 
-      .logo {
-        border-radius: 50%;
-        width: 50px;
-      }
-    `}</style>
-  </div>
-)
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+
+        .links {
+          padding-top: 15px;
+        }
+
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+
+          max-width: 800px;
+          margin-top: 3rem;
+        }
+
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+
+        .button--green {
+          display: inline-block;
+          border-radius: 4px;
+          border: 1px solid #3b8070;
+          color: #3b8070;
+          text-decoration: none;
+          padding: 10px 30px;
+        }
+
+        .button--green:hover {
+          color: #fff;
+          background-color: #3b8070;
+        }
+
+        .button--grey {
+          display: inline-block;
+          border-radius: 4px;
+          border: 1px solid #35495e;
+          color: #35495e;
+          text-decoration: none;
+          padding: 10px 30px;
+          margin-left: 15px;
+        }
+
+        .button--grey:hover {
+          color: #fff;
+          background-color: #35495e;
+        }
+
+        .logo {
+          border-radius: 50%;
+          width: 50px;
+        }
+      `}</style>
+    </div>
+  )
+}
 
 export default Home
